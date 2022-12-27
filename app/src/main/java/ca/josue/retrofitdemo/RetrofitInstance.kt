aa1ec63491d60318dd84d237df5830ad7e5dfad1
Josue Lubaki <josuelubaki@gmail.com>
2022-12-26 23:29:23 -0500

    set time Outs on client request

--- app/src/main/java/ca/josue/retrofitdemo/RetrofitInstance.kt
+++ app/src/main/java/ca/josue/retrofitdemo/RetrofitInstance.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
     companion object {
@@ -20,6 +21,9 @@ class RetrofitInstance {
 
             val client = OkHttpClient.Builder().apply {
                 this.addInterceptor(interceptor)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(20, TimeUnit.SECONDS)
+                    .writeTimeout(25, TimeUnit.SECONDS)
             }.build()
 
             return Retrofit.Builder()
