02791be2e664c58e79227a238ddd467c87985e13
Josue Lubaki <josuelubaki@gmail.com>
2022-12-26 23:23:33 -0500

    added Logging Interceptor

--- app/src/main/java/ca/josue/retrofitdemo/RetrofitInstance.kt
+++ app/src/main/java/ca/josue/retrofitdemo/RetrofitInstance.kt
@@ -1,6 +1,8 @@
 package ca.josue.retrofitdemo
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,8 +14,17 @@ class RetrofitInstance {
                 .setLenient()
                 .create()
 
+            val interceptor = HttpLoggingInterceptor().apply {
+                this.level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val client = OkHttpClient.Builder().apply {
+                this.addInterceptor(interceptor)
+            }.build()
+
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
         }
